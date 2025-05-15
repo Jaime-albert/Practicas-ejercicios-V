248b4c9f1b6a26b931641e10fe2822cdb257b779
Jaime <jaimealberto.delagarzarivera@unidemat.edu.mx>
2025-05-15 08:44:24 -0500

    Add files via upload

---
+++ practica01.php
@@ -0,0 +1,10 @@
+<?php 
+$valor1=10;
+$valor2=10;
+
+echo " Si el primer valor es ".$valor1."...";
+echo "y e segundo valor es ".$valor2."...";
+
+echo "si se suman dan un valor de ".$valor1 + $valor2 ;
+
+?>
